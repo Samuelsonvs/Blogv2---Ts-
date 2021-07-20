5e6856d0b625f9e7a17c46f4beafaf0e70dea801
Samuelsonvs <m.sametatali@gmail.com>
2021-07-20 19:34:16 +0300

    f-c

---
+++ container/Container.tsx
@@ -0,0 +1,61 @@
+import Head from "next/head";
+import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
+import { useTheme } from "next-themes";
+import Navbar from "@/components/layouts/Navbar";
+
+interface ContainerProps {
+  children: JSX.Element
+  customMeta?: string
+}
+
+export default function Container({children, ...customMeta}: ContainerProps): JSX.Element {
+  const [mounted, setMounted] = useState(false);
+  const { resolvedTheme, setTheme } = useTheme();
+
+  // After mounting, we have access to the theme
+  useEffect(() => setMounted(true), []);
+
+  const router = useRouter();
+  const meta = {
+    title: "Mert Samet Atalı – Developer, creator.",
+    description: `Front-end developer, JavaScript enthusiast.`,
+    image: "",
+    type: "website",
+    ...customMeta,
+    date: "02.02.02"
+  };
+
+  return (
+    <div className="bg-white dark:bg-black">
+      <Head>
+        <title>{meta.title}</title>
+        <meta name="robots" content="follow, index" />
+        <meta content={meta.description} name="description" />
+        <meta
+          property="og:url"
+          content={`https://msa-blog.vercel.app${router.asPath}`}
+        />
+        <link
+          rel="canonical"
+          href={`https://msa-blog.vercel.app${router.asPath}`}
+        />
+        <meta property="og:type" content={meta.type} />
+        <meta property="og:site_name" content="Mert Samet" />
+        <meta property="og:description" content={meta.description} />
+        <meta property="og:title" content={meta.title} />
+        <meta property="og:image" content={meta.image} />
+        {meta.date && (
+          <meta property="article:published_time" content={meta.date} />
+        )}
+      </Head>
+      <Navbar themeFunc={setTheme} mounted={mounted} resolvedTheme={resolvedTheme} />
+
+      <main className="bg-white dark:bg-black px-4">
+        {children}
+      </main>
+      <footer>
+      </footer>
+    </div>
+  );
+}

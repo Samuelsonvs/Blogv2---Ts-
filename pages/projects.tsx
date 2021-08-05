fad060eabda944ca8185eec0f22b2868acc0fe89
Samuelsonvs <m.sametatali@gmail.com>
2021-08-05 10:12:04 +0300

    image opt

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 import Image from "next/image";
 
 import Container from "@/container/Container";
@@ -10,24 +10,30 @@ export default function projects() {
       <div>
         {projectsDatas.map((data, idx) => {
           return (
-            <div key={idx} className="flex flex-col md:flex-row justify-between">
-              <Image
-                layout="fixed"
-                src={data.url}
-                alt="github-icon"
-                width={500}
-                height={500}
-              />
-              <ul className="md:mt-28 flex flex-row md:flex-col flex-wrap">
-                {data.tags.map((tag, index) => {
-                  return (
-                    <li key={index} className="mt-2">
-                      {tag}
-                    </li>
-                  );
-                })}
-              </ul>
-            </div>
+            <Fragment key={idx}>
+              <div className="flex flex-col md:flex-row justify-center items-center md:justify-between">
+                <div>
+                  <Image
+                    src={data.url}
+                    alt="github-icon"
+                    width={500}
+                    height={500}
+                  />
+                </div>
+                <ul className="md:mt-28 -mt-10 flex flex-row md:flex-col flex-wrap">
+                  {data.tags.map((tag, index) => {
+                    return (
+                      <li key={index} className="mt-2 px-5">
+                        {tag}
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+              <div className="mt-10 flex justify-center">
+                <p>{data.explanation}</p>
+              </div>
+            </Fragment>
           );
         })}
       </div>

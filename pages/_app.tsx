21fe25bdc324cffddfffbaee6121cbeac4fe750f
Samuelsonvs <m.sametatali@gmail.com>
2021-09-07 11:16:26 +0300

    added GA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
+import * as gtag from '../lib/gtag'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url:string) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
   return (
     <ThemeProvider attribute="class">
       <Component {...pageProps} />
